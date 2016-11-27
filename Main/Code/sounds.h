2974965f2f7e29a635764fa48da2894ae229d53c
hero24 <hero24@interia.pl>
2016-11-27 17:45:52 +0000

    Update sounds.h

--- Main/Code/sounds.h
+++ Main/Code/sounds.h
@@ -18,19 +18,18 @@ static int beat = 0;
 static long duration = 0;
 static int hornpipe_tune[] = {Col_g4, Col_a5, Col_d4, Col_g4, Col_c4, Col_d4, Col_g4, Col_d4, Col_g4, Col_g4, Col_c4, Col_a5, Col_d4,  Col_g4, Col_c4, Col_d4, Col_g4};
 static int hornpipe_beats[] = {40, 20, 20, 20, 20, 10, 8, 10, 8, 20, 20, 20, 20, 10, 10, 10, 10};
-static int hornpipe_MAXIMUM_COUNT = sizeof(hornpipe_tune) / 2; // Tune length i.o.t. loop
+static int hornpipe_MAXIMUM_COUNT = sizeof(hornpipe_tune) / sizeof(int); // Tune length i.o.t. loop
 
 static int colmans_tune[] = {Col_c4, Col_g4, Col_d4, Col_f4, Col_a4, Col_c5, Col_b4, Col_e4, Col_c4, Col_g4, Col_d4, Col_f4, Col_a4, Col_c5, Col_b4, Col_e4};
 static int colmans_beats[] = {30, 16, 20, 10, 40, 20, 60, 20, 40, 10, 40, 20, 30, 10, 60, 20};
-static int colmans_MAXIMUM_COUNT = sizeof(colmans_tune) / 2;
+static int colmans_MAXIMUM_COUNT = sizeof(colmans_tune) / sizeof(int);
 
 static int StartingBuzzer_tune[] = {Col_c5, Col_b4m, Col_a4m, Col_e4, Col_c4};
 static int StartingBuzzer_beats[] = {20, 20, 20, 20, 20};
-static int StartingBuzzer_MAXIMUM_COUNT = sizeof(StartingBuzzer_tune) / 2; 
+static int StartingBuzzer_MAXIMUM_COUNT = sizeof(StartingBuzzer_tune) / sizeof(int); 
 // tempo
 #define tempo 30000 // Ver3 for Greensleeves original tempo lengthened from 10000 to 30000 
 // length of pause
 #define pause 5000 // original pause increased from 1000 to 5000
 // add to loop to pause for 1 sec
 static int rest_count = 100;
-
